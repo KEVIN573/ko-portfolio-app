c40aa0233ce4ecc99cdaeeec28c08a488fd0fb73
Kevin Ochieng <kevinochieng932@gmail.com>
2020-07-11 14:00:20 -0700

    modified files

--- src/components/ContactAddress/ContactAddress.jsx
+++ src/components/ContactAddress/ContactAddress.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
-import { FaPhone, FaMapMarkerAlt, FaRegEnvelope} from "react-icons/fa";
-import '../ContactAddress/ContactAddress.css'
+import { FaPhone, FaMapMarkerAlt, FaRegEnvelope } from 'react-icons/fa';
+import '../ContactAddress/ContactAddress.css';
 
+//Address Component
 const Address = () => {
-    return (  
-        <React.Fragment>
+  return (
+    <React.Fragment>
+      <div className='contact-info'>
+        <span>
+          <FaPhone />
+        </span>
+        <div className='info'>
+          <h4>Phone : </h4>
+          <p>+254 758231661</p>
+        </div>
+      </div>
 
-            <div className="contact-info">
-                <span><FaPhone /></span>
-                <div className="info">
-                    <h4>Phone : </h4>
-                    <p>+254 758231661</p>
-                </div>
-            </div>
-            
-        
-            <div className="contact-info">
-                <span><FaMapMarkerAlt /></span>
-                <div className="info">
-                    <h4>Residence : </h4>
-                    <p>Kenya, Kisumu</p>
-                </div>
-            </div>
-        
-            <div className="contact-info">
-                <span><FaRegEnvelope /></span>
-                <div className="info">
-                    <h4>Email : </h4>
-                    <p>kevinohkelvin932@gmail.com</p>
-                </div>
-            </div>
+      <div className='contact-info'>
+        <span>
+          <FaMapMarkerAlt />
+        </span>
+        <div className='info'>
+          <h4>Residence : </h4>
+          <p>Kenya, Kisumu</p>
+        </div>
+      </div>
 
-        </React.Fragment>    
-    );
-}
- 
+      <div className='contact-info'>
+        <span>
+          <FaRegEnvelope />
+        </span>
+        <div className='info'>
+          <h4>Email : </h4>
+          <p>
+            <a href = 'https://mail.google.com/' >kevinohkelvin932@gmail.com </a>
+          </p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
 export default Address;
